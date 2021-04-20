9f4ff76610899dc65fd86d850143b69979438cb6
Lucas Larisch <lucas.larisch@googlemail.com>
2021-04-21 01:15:55 +0200

    Added shell script for clean installing components in correct order / ignored target directories.

---
+++ cleaninstalleach.sh
@@ -0,0 +1,10 @@
+echo "Building modules in correct order..."
+
+mvn clean install -f user-administration/pom.xml
+mvn clean install -f score-administration/pom.xml
+mvn clean install -f authorization/pom.xml
+mvn clean install -f vocabulary-administration/pom.xml
+mvn clean install -f game-administration/pom.xml
+mvn clean install -f round-administration/pom.xml
+
+echo "Clean install finished!"
