1317a2257f3e5a4c8ea166658acb493d29a96565
Lucas Larisch <lucas.larisch@googlemail.com>
2021-04-21 05:01:54 +0200

    Improved user info echos in Shell script.

--- cleaninstalleach.sh
+++ cleaninstalleach.sh
@@ -1,4 +1,4 @@
-echo "Building modules in correct order..."
+echo "Building modules in correct order...Please make sure you have called this Shell script from no different directory than the repository root or else it will lead to nothing but avoidable frustration due to failed operations."
 
 mvn clean install -f user-administration/pom.xml
 mvn clean install -f score-administration/pom.xml
@@ -6,4 +6,4 @@ mvn clean install -f vocabulary-administration/pom.xml
 mvn clean install -f game-administration/pom.xml
 mvn clean install -f round-administration/pom.xml
 
-echo "Clean install finished!"
+echo "Clean install finished. In case of doubt, please check the logs above yourself in order to find out whether the operations were successful or failed."
