c82eb824d0cdca98194c83d4fc47237e9bc55031
Lucas Larisch <lucas.larisch@googlemail.com>
2021-04-21 16:30:06 +0200

    Added db module => dependency of each other module.

--- cleaninstalleach.sh
+++ cleaninstalleach.sh
@@ -1,5 +1,6 @@
 echo "Building modules in correct order...Please make sure you have called this Shell script from no different directory than the repository root or else it will lead to nothing but avoidable frustration due to failed operations."
 
+mvn clean install -f db-connector/pom.xml
 mvn clean install -f user-administration/pom.xml
 mvn clean install -f vocabulary-administration/pom.xml
 mvn clean install -f game-administration/pom.xml
