ee5ded912d33c53b65481f6b03230d7fb18cee21
Lucas Larisch <lucas.larisch@googlemail.com>
2021-04-21 05:11:36 +0200

    Split interface of vocabulary administration component.

--- vocabulary-administration/src/main/java/de/htwberlin/kba/gr7/VocabularyLib.java
+++ vocabulary-administration/src/main/java/de/htwberlin/kba/gr7/VocabularyLib.java
@@ -1,25 +1,14 @@
 package de.htwberlin.kba.gr7;
 
-import de.htwberlin.kba.gr7.exceptions.DuplicateVocablesInSet;
-import de.htwberlin.kba.gr7.exceptions.IncompleteVocableSetInformation;
 import de.htwberlin.kba.gr7.exceptions.UnauthorizedException;
 import de.htwberlin.kba.gr7.model.SupportedLanguage;
 import de.htwberlin.kba.gr7.model.User;
 import de.htwberlin.kba.gr7.model.VocableSet;
 
-import java.io.File;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface VocabularyAdministration {
-    int insertVocableSet(VocableSet vocables) throws DuplicateVocablesInSet, IncompleteVocableSetInformation, UnauthorizedException, SQLException;
-
-    int updateVocableSet(VocableSet vocables) throws DuplicateVocablesInSet, IncompleteVocableSetInformation, UnauthorizedException, SQLException;
-
-    int deleteVocableSet(VocableSet vocables) throws UnauthorizedException, SQLException;
-
-    int importGnuVocableSet(File gnuFile) throws DuplicateVocablesInSet, IncompleteVocableSetInformation, UnauthorizedException, SQLException;
-
+public interface VocabularyLib {
     VocableSet getVocableSet(int id) throws UnauthorizedException, SQLException;
 
     List<VocableSet> getVocableSetsOfUser(User user) throws UnauthorizedException, SQLException;
