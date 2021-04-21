da060c9530509b4deb251d9bd8a279fc4edaf404
Lucas Larisch <lucas.larisch@googlemail.com>
2021-04-21 04:03:27 +0200

    Added exceptions to score administration.

--- score-administration/src/main/java/de/htwberlin/kba/gr7/ScoreAdministration.java
+++ score-administration/src/main/java/de/htwberlin/kba/gr7/ScoreAdministration.java
@@ -1,17 +1,19 @@
 package de.htwberlin.kba.gr7;
 
+import de.htwberlin.kba.gr7.exceptions.UnauthorizedException;
 import de.htwberlin.kba.gr7.model.GameScore;
 import de.htwberlin.kba.gr7.model.PersonalGameScore;
 import de.htwberlin.kba.gr7.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ScoreAdministration {
-    List<PersonalGameScore> getPersonalGameScores(User user);
+    List<PersonalGameScore> getPersonalGameScores(User user) throws SQLException, UnauthorizedException;
 
-    int getTotalWinsOfUser(User user);
+    int getTotalWinsOfUser(User user) throws SQLException, UnauthorizedException;
 
-    int getTotalLossesOfUser(User user);
+    int getTotalLossesOfUser(User user) throws SQLException, UnauthorizedException;
 
-    int insertScore(GameScore gameScore);
+    int insertScore(GameScore gameScore) throws SQLException, UnauthorizedException;
 }
