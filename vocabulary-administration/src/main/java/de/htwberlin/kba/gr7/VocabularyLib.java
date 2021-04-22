318f76de9111b8aaa522c06db20a25f5771fb36c
Lucas Larisch <lucas.larisch@googlemail.com>
2021-04-22 17:14:04 +0200

    Added functions for more precisely searching for vocable sets.

--- vocabulary-administration/src/main/java/de/htwberlin/kba/gr7/VocabularyLib.java
+++ vocabulary-administration/src/main/java/de/htwberlin/kba/gr7/VocabularyLib.java
@@ -15,15 +15,31 @@ public interface VocabularyLib {
 
     List<VocableSet> findVocableSetsByString(String searchString) throws UnauthorizedException, SQLException;
 
+    List<VocableSet> findVocableSetsByTitle(String titleSearchString) throws UnauthorizedException, SQLException;
+
+    List<VocableSet> findVocableSetsBySourceTitle(String sourceTitleSearchString) throws UnauthorizedException, SQLException;
+
     List<VocableSet> findVocableSetsWithKnownLanguage(SupportedLanguage language) throws UnauthorizedException, SQLException;
 
-    List<VocableSet> findVocableSetsWithKnownLanguage(SupportedLanguage language, String searchString) throws UnauthorizedException, SQLException;
+    List<VocableSet> findVocableSetsWithKnownLanguageByString(SupportedLanguage language, String searchString) throws UnauthorizedException, SQLException;
+
+    List<VocableSet> findVocableSetsWithKnownLanguageByTitle(SupportedLanguage language, String titleSearchString) throws UnauthorizedException, SQLException;
+
+    List<VocableSet> findVocableSetsWithKnownLanguageBySourceTitle(SupportedLanguage language, String sourceTitleSearchString) throws UnauthorizedException, SQLException;
 
     List<VocableSet> findVocableSetsWithLearntLanguage(SupportedLanguage language) throws UnauthorizedException, SQLException;
 
-    List<VocableSet> findVocableSetsWithLearntLanguage(SupportedLanguage language, String searchString) throws UnauthorizedException, SQLException;
+    List<VocableSet> findVocableSetsWithLearntLanguageByString(SupportedLanguage language, String searchString) throws UnauthorizedException, SQLException;
 
-    List<VocableSet> findVocableSetsWithLanguage(SupportedLanguage known, SupportedLanguage learnt) throws UnauthorizedException, SQLException;
+    List<VocableSet> findVocableSetsWithLearntLanguageByTitle(SupportedLanguage language, String titleSearchString) throws UnauthorizedException, SQLException;
 
-    List<VocableSet> findVocableSetsWithLanguage(SupportedLanguage known, SupportedLanguage learnt, String searchString) throws UnauthorizedException, SQLException;
+    List<VocableSet> findVocableSetsWithLearntLanguageBySourceTitle(SupportedLanguage language, String sourceTitleSearchString) throws UnauthorizedException, SQLException;
+
+    List<VocableSet> findVocableSetsWithLanguages(SupportedLanguage known, SupportedLanguage learnt) throws UnauthorizedException, SQLException;
+
+    List<VocableSet> findVocableSetsWithLanguagesByString(SupportedLanguage known, SupportedLanguage learnt, String searchString) throws UnauthorizedException, SQLException;
+
+    List<VocableSet> findVocableSetsWithLanguagesByTitle(SupportedLanguage known, SupportedLanguage learnt, String titleSearchString) throws UnauthorizedException, SQLException;
+
+    List<VocableSet> findVocableSetsWithLanguagesBySourceTitle(SupportedLanguage known, SupportedLanguage learnt, String sourceTitleSearchString) throws UnauthorizedException, SQLException;
 }
