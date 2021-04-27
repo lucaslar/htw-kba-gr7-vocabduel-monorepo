48e0945fab7b0443b881029d8c5260551ce998b3
Lucas Larisch <lucas.larisch@googlemail.com>
2021-04-27 19:29:09 +0200

    Reduced number of supported languages due to resulting in too large UML diagram.

--- vocabulary-administration/src/main/java/de/htwberlin/kba/gr7/vocabduel/vocabulary/model/SupportedLanguage.java
+++ vocabulary-administration/src/main/java/de/htwberlin/kba/gr7/vocabduel/vocabulary/model/SupportedLanguage.java
@@ -1,11 +1,19 @@
 package de.htwberlin.kba.gr7.vocabduel.vocabulary.model;
 
 public enum SupportedLanguage {
-    // see https://en.wiktionary.org/wiki/Index:All_languages
-    EN, AB, ADY, AF, AK, SQ, ASE, AM, AR, AN, ARC, HY, AS, AY, AZ, BAN, EU, BEW, SH, BR, BG, YUE, CA, CHR, CLC, ZH, COP,
-    KW, CO, CRH, CS, DA, NL, DWR, EO, ET, EE, HIF, FIL, FI, FR, GL, KA, DE, EL, GRC, KL, HT, HAW, HE, HI, HU, IS, ID, IU,
-    IA, GA, IT, JA, JV, KBD, KLS, KN, CSB, KM, RW, KO, KU, LLD, LTG, LA, LN, LIV, JBO, DSB, NDS, MK, MS, ML, CMN, GV, MI,
-    MFE, ENM, GML, NAN, MN, NO, XCL, ANG, FRO, KAW, NON, PRG, OR, PAG, PAP, PS, FA, PJT, PL, PT, SLA, PA, QYA, RAJ, RAP,
-    RO, RU, SA, SCO, GD, SEA, SK, SL, ES, SI, SW, SV, TL, TG, TA, ROA, TE, TH, TPI, TR, UK, HSB, UR, UG, UZ, VEC, VI,
-    WYM, VO, VRO, CY, XH, YI, ZZA, ZU
+    // Alphabetically sorted most important languages according to:
+    // https://www.uopeople.edu/blog/most-important-languages-to-learn/
+
+    AR, // Arabic
+    CMN, // Mandarin
+    DE, // German
+    EN, // English
+    ES, // Spanish
+    FR, // French
+    HI, // Hindi
+    IT, // Italian
+    JA, // Japanese
+    KO, // Korean
+    PT, // Portuguese
+    RU, // Russian
 }
