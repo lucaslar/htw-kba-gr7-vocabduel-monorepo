0608fdfccdf10943014deb79ba7f3e817ef71906
Lucas Larisch <lucas.larisch@googlemail.com>
2021-05-02 18:39:00 +0200

    Kebab case => snake case.

--- cleaninstalleach.sh
+++ cleaninstalleach.sh
@@ -1,7 +1,7 @@
 echo "Building modules in correct order...Please make sure you have called this Shell script from no different directory than the repository root or else it will lead to nothing but avoidable frustration due to failed operations."
 
-mvn clean install -f user-administration/pom.xml
-mvn clean install -f vocabulary-administration/pom.xml
-mvn clean install -f game-administration/pom.xml
+mvn clean install -f user_administration/pom.xml
+mvn clean install -f vocabulary_administration/pom.xml
+mvn clean install -f game_administration/pom.xml
 
 echo "Clean install finished. In case of doubt, please check the logs above yourself in order to find out whether the operations were successful or failed."
