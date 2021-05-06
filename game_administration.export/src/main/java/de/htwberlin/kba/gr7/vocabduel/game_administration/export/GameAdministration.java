ba67b1f5862a336fe926b0d0705c7206944fb41c
Lucas Larisch <lucas.larisch@googlemail.com>
2021-05-06 02:06:27 +0200

    Added JavaDoc for game_administratoin.export.

--- game_administration.export/src/main/java/de/htwberlin/kba/gr7/vocabduel/game_administration/export/GameAdministration.java
+++ game_administration.export/src/main/java/de/htwberlin/kba/gr7/vocabduel/game_administration/export/GameAdministration.java
@@ -4,39 +4,59 @@ import de.htwberlin.kba.gr7.vocabduel.game_administration.export.model.CorrectAn
 import de.htwberlin.kba.gr7.vocabduel.game_administration.export.model.VocabduelGame;
 import de.htwberlin.kba.gr7.vocabduel.game_administration.export.model.VocabduelRound;
 import de.htwberlin.kba.gr7.vocabduel.user_administration.export.model.User;
+import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.export.model.SupportedLanguage;
+import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.export.model.TranslationGroup;
+import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.export.model.VocableList;
 
 import java.util.List;
 
+/**
+ * Service for managing current games and their rounds.
+ *
+ * @author Sebastian Kehl, Lucas Larisch
+ * @version 1.0, May 2021
+ */
 public interface GameAdministration {
 
     /**
-     * start a new game
-     * @param playerA <code>User</code>
-     * @param playerB <code>User</code>
-     * @return new <code>VocabduelGame</code>
+     * Starts a new game as <code>playerA</code> against <code>playerB</code> with the given parameters
+     * (languages/vocable lists) and returns an instance of that newly started match.
+     *
+     * @param playerA        Player initiating the game.
+     * @param playerB        Opponent to be invited to play with <code>playerA</code>.
+     * @param vocableLists   List of vocable lists to be used in the game.
+     * @param knownLanguage  Known language, i.e. the language in which the questions are to be asked in.
+     * @param learntLanguage Learnt language, i.e. the language the answers are displayed in.
+     * @return New <code>VocabduelGame</code> instance based on the given params.
      */
     VocabduelGame startGame(User playerA, User playerB, List<VocableList> vocableLists, SupportedLanguage knownLanguage, SupportedLanguage learntLanguage);
 
     /**
-     * get list of challenged games from one user
-     * @param user <code>User</code>
-     * @return
+     * Collects all pending, i.e. not finished, games a given user has been challenged to and returns them.
+     *
+     * @param user User the pending/challenged matches of are to be returned.
+     * @return List of all unfinished games the given <code>user</code> has been challenged to.
      */
     List<VocabduelGame> getPersonalChallengedGames(User user);
 
     /**
-     * start next round of a game.
-     * @param player
-     * @param game
-     * @return
+     * Starts the next round of a given game as a given player, i.e. returns the respective next round object.
+     *
+     * @param player Player the next round is to be returned for.
+     * @param game   Game the next round of is to be returned for the given <code>user</code>.
+     * @return New <code>VocabduelRound</code> of a given game for a given user.
      */
     VocabduelRound startRound(User player, VocabduelGame game);
 
     /**
-     * answer question in a round of a game
-     * @param player <code>User</code>
-     * @param game <code>VocabduelGame</code>
-     * @return <code>CorrectAnswerResult</code>
+     * Checks and stores the result for an answer submitted in a given <code>round</code> by a given
+     * <code>player</code> and returns feedback for that answer incl. the correct answer in case of
+     * having submitted a wrong one.
+     *
+     * @param player Player who has answered the question.
+     * @param round  Round the answer has been submitted for.
+     * @param answer Answer submitted by the given <code>player</code>.
+     * @return Result for the given <code>round</code> from the perspective of the given <code>player</code> incl. the correct answer in case of having submitted a wrong one.
      */
     CorrectAnswerResult answerQuestion(User player, VocabduelRound round, TranslationGroup answer);
 }
--- game_administration.export/src/main/java/de/htwberlin/kba/gr7/vocabduel/game_administration/export/ScoreAdministration.java
+++ game_administration.export/src/main/java/de/htwberlin/kba/gr7/vocabduel/game_administration/export/ScoreAdministration.java
@@ -6,35 +6,51 @@ import de.htwberlin.kba.gr7.vocabduel.user_administration.export.model.User;
 
 import java.util.List;
 
+/**
+ * Service for managing game scores, i.e. both registering them and accessing historic game scores.
+ *
+ * @author Sebastian Kehl, Lucas Larisch
+ * @version 1.0, May 2021
+ */
 public interface ScoreAdministration {
 
     /**
-     * get list of finished games with game data and game scores
-     * @param user
-     * @return
+     * Collects and returns the games finished by a given user, personalized in respect of this player's view (e.g.
+     * result = Two points for the given player, one point for its opponent &rarr; return an object marked as a
+     * <code>WIN</code>). The objects of this list contain easier interpretable data than just a plain score by
+     * including further relevant meta data.
+     *
+     * @param user User whose finished games are to be returned and for whom they are to be personalized.
+     * @return List of finished games including result data from the given player's point of view.
      */
     List<PersonalFinishedGame> getPersonalGameScores(User user);
 
     /**
-     * get total wins
-     * @param user
-     * @return
+     * Counts and returns the won games of a given player.
+     *
+     * @param user User the wins of are to be counted.
+     * @return Total number of the given player's wins.
      */
     int getTotalWinsOfUser(User user);
 
     /**
-     * get total losses
-     * @param user
-     * @return
+     * Counts and returns the lost games of a given player.
+     *
+     * @param user User the losses of are to be counted.
+     * @return Total number of the given player's losses.
      */
     int getTotalLossesOfUser(User user);
 
     /**
-     * finish game
-     * @param player
-     * @param game
-     * @return
+     * Finishes a game, i.e. determines its final score and stores it. Afterwards, the finished game including its
+     * result personalized from the given player's point of view including easily interpretable meta data is returned
+     * (e.g. result = Two points for the given player, one point for its opponent &rarr; return an object marked as a
+     * <code>WIN</code>).
+     *
+     * @param user User the finished game incl. result is to be personalized for.
+     * @param game Game the final score of is to be determined and stored.
+     * @return Finished game including result and meta-data from the given player's point of view.
      */
-    PersonalFinishedGame finishGame(User player, VocabduelGame game);
+    PersonalFinishedGame finishGame(User user, VocabduelGame game);
     // TODO: sollte finish game nicht in game administration rein?
 }
