10e09c81412bc79f6ea89106e84ff7e2f6620de9
Rotkehlchen19 <sebastiand.kehl@gmail.com>
2021-05-09 01:17:11 +0200

    tests for gameAdminPersonalChallengedGames: removed unnecessary exception, add mockito

--- game_administration/src/test/java/de/htwberlin/kba/gr7/vocabduel/game_administration/GameAdministrationImplUnfinishedGamesTest.java
+++ game_administration/src/test/java/de/htwberlin/kba/gr7/vocabduel/game_administration/GameAdministrationImplUnfinishedGamesTest.java
@@ -1,48 +1,45 @@
 package de.htwberlin.kba.gr7.vocabduel.game_administration;
 
-import de.htwberlin.kba.gr7.vocabduel.game_administration.export.exceptions.NotFinishedGameGivenMoreThanOnceException;
 import de.htwberlin.kba.gr7.vocabduel.game_administration.export.model.VocabduelGame;
 import de.htwberlin.kba.gr7.vocabduel.user_administration.export.model.User;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 
 public class GameAdministrationImplUnfinishedGamesTest {
 
-    private User user;
-    private GameAdministrationImpl gameAdministration;
     private List<VocabduelGame> unfinishedGames;
 
     @Before
     public void setup() {
-        // TODO: mocking, so tests about isEmpty() and data in personalChallengedGames can be tested??
-        user = new User(2020L);
-        gameAdministration = new GameAdministrationImpl();
+        User user = new User(2020L);
+        GameAdministrationImpl gameAdministration = Mockito.mock(GameAdministrationImpl.class);
+        Mockito.when(gameAdministration.getPersonalChallengedGames(
+                user)).thenReturn(null); // TODO: Continue with implementation - Mock should return server response for unfinished game
         unfinishedGames = gameAdministration.getPersonalChallengedGames(user);
+        Assert.assertNotNull(unfinishedGames);
     }
 
     @Test()
     public void shouldGetEmptyChallengedGames(){
-        unfinishedGames = gameAdministration.getPersonalChallengedGames(user);
         Assert.assertTrue(unfinishedGames.isEmpty());
     }
 
-    // TODO: exception richtig gesetzt?
     @Test()
-    public void shouldGetEveryGameOnce() throws NotFinishedGameGivenMoreThanOnceException {
-        Collections.sort(unfinishedGames, compareById);
+    public void shouldGetEveryGameOnce() {
+        unfinishedGames.sort(compareById);
         final long[] gameId = {0};
         unfinishedGames.forEach(game -> {
             if (gameId[0] == 0 || gameId[0] != game.getId()) gameId[0] = game.getId();
-            else new NotFinishedGameGivenMoreThanOnceException();
+            else Assert.fail();
         });
     }
 
-    Comparator<VocabduelGame> compareById = (VocabduelGame game1, VocabduelGame game2) -> game1.getId().compareTo(game2.getId());
+    Comparator<VocabduelGame> compareById = Comparator.comparing(VocabduelGame::getId);
 
 }
