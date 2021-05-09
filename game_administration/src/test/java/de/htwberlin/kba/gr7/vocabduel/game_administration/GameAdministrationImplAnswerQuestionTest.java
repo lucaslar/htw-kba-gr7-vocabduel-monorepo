7f4b37303240128bdecc7a3240c56a935ad14177
Rotkehlchen19 <sebastiand.kehl@gmail.com>
2021-05-09 18:55:08 +0200

    finalize tests for gameAdminAnswerQuestion

--- game_administration/src/test/java/de/htwberlin/kba/gr7/vocabduel/game_administration/GameAdministrationImplAnswerQuestionTest.java
+++ game_administration/src/test/java/de/htwberlin/kba/gr7/vocabduel/game_administration/GameAdministrationImplAnswerQuestionTest.java
@@ -1,54 +1,37 @@
 package de.htwberlin.kba.gr7.vocabduel.game_administration;
 
+import de.htwberlin.kba.gr7.vocabduel.game_administration.assets.GameAdministrationMock;
 import de.htwberlin.kba.gr7.vocabduel.game_administration.export.model.CorrectAnswerResult;
 import de.htwberlin.kba.gr7.vocabduel.game_administration.export.model.Result;
-import de.htwberlin.kba.gr7.vocabduel.game_administration.export.model.VocabduelRound;
-import de.htwberlin.kba.gr7.vocabduel.user_administration.export.model.User;
-import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.export.model.TranslationGroup;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 public class GameAdministrationImplAnswerQuestionTest {
 
     private GameAdministrationImpl gameAdministration;
-
-    private User user;
-    private VocabduelRound newRound;
+    private GameAdministrationMock mock;
 
     @Before
     public void setup(){
-        gameAdministration = Mockito.mock(GameAdministrationImpl.class);
-        user = new User(2020L);
-
-        // set object VocabduelRound
-        newRound = new VocabduelRound();
-        newRound.setAnswers(Arrays.asList(
-                new TranslationGroup(Collections.singletonList("wrongAnswer1")),
-                new TranslationGroup(Collections.singletonList("rightAnswer")),
-                new TranslationGroup(Collections.singletonList("wrongAnswer2")),
-                new TranslationGroup(Collections.singletonList("wrongAnswer3"))
-        ));
+        gameAdministration = new GameAdministrationImpl();
+        mock = new GameAdministrationMock();
     }
 
     @Test()
     public void shouldGetLossWithIncorrectAnswer(){
         CorrectAnswerResult result = gameAdministration.answerQuestion(
-                user, newRound, newRound.getAnswers().get(2));
+                mock.mockSampleUser(), mock.mockVocabduelRound(), mock.mockVocabduelRound().getAnswers().get(2));
         Assert.assertNotNull(result);
         Assert.assertEquals(Result.LOSS, result.getResult());
         Assert.assertNotNull(result.getCorrectAnswer());
-        Assert.assertEquals(result.getCorrectAnswer(), newRound.getAnswers().get(1));
+        Assert.assertEquals(result.getCorrectAnswer(), mock.mockVocabduelRound().getAnswers().get(1));
     }
 
     @Test()
     public void shouldGetWinWithCorrectAnswer(){
         CorrectAnswerResult result = gameAdministration.answerQuestion(
-                user, newRound, newRound.getAnswers().get(1));
+                mock.mockSampleUser(), mock.mockVocabduelRound(), mock.mockVocabduelRound().getAnswers().get(1));
         Assert.assertNotNull(result);
         Assert.assertEquals(Result.WIN, result.getResult());
         Assert.assertNull(result.getCorrectAnswer());
