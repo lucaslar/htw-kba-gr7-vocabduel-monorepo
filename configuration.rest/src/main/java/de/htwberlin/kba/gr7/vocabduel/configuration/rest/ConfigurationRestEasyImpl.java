919e50445a2b8ee73a5de92cb5578aab362d2f1c
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-15 23:17:23 +0200

    Renamed RestEasy config.

--- configuration.rest/src/main/java/de/htwberlin/kba/gr7/vocabduel/configuration/rest/ConfigurationRestEasyImpl.java
+++ configuration.rest/src/main/java/de/htwberlin/kba/gr7/vocabduel/configuration/rest/ConfigurationRestEasyImpl.java
@@ -4,6 +4,6 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/vocabduel")
-public class RestEasyServices extends Application {
+public class ConfigurationRestEasyImpl extends Application {
     // All services are initialized automatically
 }
