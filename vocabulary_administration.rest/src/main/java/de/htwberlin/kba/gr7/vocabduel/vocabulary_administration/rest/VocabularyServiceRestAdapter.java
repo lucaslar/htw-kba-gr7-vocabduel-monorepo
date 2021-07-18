e1f95f772cc7644ad333e2a35e043a99dc73ef33
Rotkehlchen19 <sebastiand.kehl@gmail.com>
2021-07-18 02:53:06 +0200

    implement REST paths for vocabService

--- vocabulary_administration.rest/src/main/java/de/htwberlin/kba/gr7/vocabduel/vocabulary_administration/rest/VocabularyServiceRestAdapter.java
+++ vocabulary_administration.rest/src/main/java/de/htwberlin/kba/gr7/vocabduel/vocabulary_administration/rest/VocabularyServiceRestAdapter.java
@@ -1,11 +1,18 @@
 package de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.rest;
 
+import de.htwberlin.kba.gr7.vocabduel.user_administration.export.model.User;
 import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.export.VocabularyService;
+import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.export.exceptions.*;
+import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.export.model.LanguageSet;
+import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.export.model.SupportedLanguage;
+import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.export.model.VocableList;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Controller
 @Path("/vocabulary")
@@ -18,11 +25,119 @@ public class VocabularyServiceRestAdapter {
         VOCABULARY_SERVICE = vocabularyService;
     }
 
-    @GET
-    @Path("/hello")
-    public String hello() {
-        System.out.println("There's something happening here...");
-        System.out.println("Vocabulary Service is" + (VOCABULARY_SERVICE == null ? "n't" : "") + " initialized");
-        return "Hello REST world!";
+    @POST
+    @Path("/import-list")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public Response importGnuVocableList(final String gnuContent, final User user){
+        try{
+            VOCABULARY_SERVICE.importGnuVocableList(gnuContent, user);
+        } catch (IncompleteVocableListException | DataAlreadyExistsException |
+                InvalidVocableListException | UnknownLanguagesException |
+                DuplicateVocablesInSetException e) {
+            e.printStackTrace();
+            return Response
+                    .status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(e.getMessage())
+                    .type(MediaType.TEXT_PLAIN_TYPE)
+                    .build();
+        }
+        System.out.println("Successfully imported GnuVocableList.");
+        return Response
+                .status(Response.Status.CREATED)
+                .type(MediaType.TEXT_PLAIN_TYPE)
+                .build();
     }
+
+
+    @POST
+    @Path("/delete-list")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public Response deleteVocableList(final VocableList vocableList, final User user){
+        try{
+            VOCABULARY_SERVICE.deleteVocableList(vocableList, user);
+        } catch (DifferentAuthorException e) {
+            e.printStackTrace();
+            return Response
+                    .status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(e.getMessage())
+                    .type(MediaType.TEXT_PLAIN_TYPE)
+                    .build();
+        }
+        System.out.println("Successfully deleted VocableList.");
+        return Response
+                .status(Response.Status.OK)
+                .type(MediaType.TEXT_PLAIN_TYPE)
+                .build();
+    }
+
+    @GET
+    @Path("/get-list")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public Response getVocableListById(final long id){
+        VocableList list = VOCABULARY_SERVICE.getVocableListById(id);
+        return Response
+                .status(Response.Status.OK)
+                .entity(list)
+                .type(MediaType.TEXT_PLAIN_TYPE)
+                .build();
+    }
+
+    @GET
+    @Path("/get-lists")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public Response getVocableListsByUser(final User user){
+        List<VocableList> lists = VOCABULARY_SERVICE.getVocableListsOfUser(user);
+        return Response
+                .status(Response.Status.OK)
+                .entity(lists)
+                .type(MediaType.TEXT_PLAIN_TYPE)
+                .build();
+    }
+
+    @GET
+    @Path("/get-lang-sets")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public Response getAllLanguageSets(){
+        List<LanguageSet> sets = VOCABULARY_SERVICE.getAllLanguageSets();
+        return Response
+                .status(Response.Status.OK)
+                .entity(sets)
+                .type(MediaType.TEXT_PLAIN_TYPE)
+                .build();
+    }
+
+    @GET
+    @Path("/get-languages")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public Response getAllSupportedLanguages(){
+        List<SupportedLanguage> languages = VOCABULARY_SERVICE.getAllSupportedLanguages();
+        return Response
+                .status(Response.Status.OK)
+                .entity(languages)
+                .type(MediaType.TEXT_PLAIN_TYPE)
+                .build();
+    }
+
+    @GET
+    @Path("/get-language-references")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public Response getSupportedLanguageReferences(final SupportedLanguage lang){
+        List<String> refs = VOCABULARY_SERVICE.getSupportedLanguageReferences(lang);
+        return Response
+                .status(Response.Status.OK)
+                .entity(refs)
+                .type(MediaType.TEXT_PLAIN_TYPE)
+                .build();
+
+    }
+
+    // TODO Remove example for auth-guarded route
+    @GET
+    @Path("/guarded")
+    public Response guardedTest () {
+        return Response.status(javax.ws.rs.core.Response.Status.OK).build();
+    }
+
 }
