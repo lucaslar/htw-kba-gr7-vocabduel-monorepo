4a8b74756256e900f894a2ce3ed9e7247c07d10d
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-20 12:33:53 +0200

    Re-added CORS filter (to be used for development only).

--- configuration.rest/src/main/java/de/htwberlin/kba/gr7/vocabduel/configuration/rest/ConfigurationRestEasyImpl.java
+++ configuration.rest/src/main/java/de/htwberlin/kba/gr7/vocabduel/configuration/rest/ConfigurationRestEasyImpl.java
@@ -6,6 +6,7 @@ import de.htwberlin.kba.gr7.vocabduel.game_administration.rest.ScoreServiceRestA
 import de.htwberlin.kba.gr7.vocabduel.user_administration.rest.AuthServiceRestAdapter;
 import de.htwberlin.kba.gr7.vocabduel.user_administration.rest.UserServiceRestAdapter;
 import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.rest.VocabularyServiceRestAdapter;
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -20,6 +21,14 @@ public class ConfigurationRestEasyImpl extends Application {
     private final Set<Object> SINGLETONS = new HashSet<>();
 
     public ConfigurationRestEasyImpl() {
+        // For local development only (if not set, requests to the same server are not prohibited):
+        // TODO Comment out when in production in order to support every port
+        CorsFilter corsFilter = new CorsFilter();
+        corsFilter.getAllowedOrigins().add("http://localhost:4200"); // local dev
+        corsFilter.getAllowedOrigins().add("http://localhost:8080"); // productive application
+        corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
+        SINGLETONS.add(corsFilter);
+
         // Auth interceptor:
         SINGLETONS.add(CONTEXT.getBean(AuthInterceptor.class));
 
