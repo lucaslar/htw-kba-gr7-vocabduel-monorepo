2c2d325280b8fb73c67c23e5d6b4b36344392d04
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-21 04:57:07 +0200

    Added round counter to minimized personal game info.

--- game_administration.rest/src/main/java/de/htwberlin/kba/gr7/vocabduel/game_administration/rest/model/MinimizedPersonalGameInfo.java
+++ game_administration.rest/src/main/java/de/htwberlin/kba/gr7/vocabduel/game_administration/rest/model/MinimizedPersonalGameInfo.java
@@ -1,5 +1,6 @@
 package de.htwberlin.kba.gr7.vocabduel.game_administration.rest.model;
 
+import de.htwberlin.kba.gr7.vocabduel.game_administration.export.GameService;
 import de.htwberlin.kba.gr7.vocabduel.game_administration.export.model.RunningVocabduelGame;
 import de.htwberlin.kba.gr7.vocabduel.user_administration.export.model.User;
 import de.htwberlin.kba.gr7.vocabduel.vocabulary_administration.export.model.SupportedLanguage;
@@ -10,17 +11,32 @@ import java.util.stream.Collectors;
 public class MinimizedPersonalGameInfo {
     private Long id;
     private User opponent;
+    private long totalRounds;
+    private long finishedRoundsSelf;
+    private long finishedRoundsOpponent;
     private SupportedLanguage knownLanguage;
     private SupportedLanguage learntLanguage;
     private List<MinimizedUsedListInfo> usedVocabularyLists;
 
     public MinimizedPersonalGameInfo(final RunningVocabduelGame game, final User personalizeFor) {
+        final boolean isSelfA = personalizeFor.getId().equals(game.getPlayerA().getId());
+        final long countA = game.getRounds().stream().filter(r -> r.getResultPlayerA() != null).count();
+        final long countB = game.getRounds().stream().filter(r -> r.getResultPlayerB() != null).count();
+
         id = game.getId();
         knownLanguage = game.getKnownLanguage();
         learntLanguage = game.getLearntLanguage();
-        opponent = personalizeFor.getId().equals(game.getPlayerA().getId()) ? game.getPlayerB() : game.getPlayerA();
         usedVocabularyLists = game.getVocableLists().stream().map(MinimizedUsedListInfo::new).collect(Collectors.toList());
-        // TODO Add rounds stats
+        totalRounds = GameService.NR_OF_ROUNDS;
+        if (isSelfA) {
+            opponent = game.getPlayerB();
+            finishedRoundsSelf = countA;
+            finishedRoundsOpponent = countB;
+        } else {
+            opponent = game.getPlayerA();
+            finishedRoundsSelf = countB;
+            finishedRoundsOpponent = countA;
+        }
     }
 
     public Long getId() {
@@ -29,6 +45,18 @@ public class MinimizedPersonalGameInfo {
 
     public User getOpponent() {
         return opponent;
+    }
+
+    public long getTotalRounds() {
+        return totalRounds;
+    }
+
+    public long getFinishedRoundsSelf() {
+        return finishedRoundsSelf;
+    }
+
+    public long getFinishedRoundsOpponent() {
+        return finishedRoundsOpponent;
     }
 
     public SupportedLanguage getKnownLanguage() {
@@ -48,6 +76,9 @@ public class MinimizedPersonalGameInfo {
         return "MinimizedPersonalGameInfo{" +
                 "id=" + id +
                 ", opponent=" + opponent +
+                ", totalRounds=" + totalRounds +
+                ", finishedRoundsSelf=" + finishedRoundsSelf +
+                ", finishedRoundsOpponent=" + finishedRoundsOpponent +
                 ", knownLanguage=" + knownLanguage +
                 ", learntLanguage=" + learntLanguage +
                 ", usedVocabularyLists=" + usedVocabularyLists +
