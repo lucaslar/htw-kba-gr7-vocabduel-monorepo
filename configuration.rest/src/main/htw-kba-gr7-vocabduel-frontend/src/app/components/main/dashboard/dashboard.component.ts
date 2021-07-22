09618b582b8d58de27ef2ff5c08eb9c31ac8927f
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-23 00:42:04 +0200

    Own imported vocabulary displayed in dashboard.

--- configuration.rest/src/main/htw-kba-gr7-vocabduel-frontend/src/app/components/main/dashboard/dashboard.component.ts
+++ configuration.rest/src/main/htw-kba-gr7-vocabduel-frontend/src/app/components/main/dashboard/dashboard.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { VocabularyService } from '../../../services/vocabulary.service';
+import { VocableList } from '../../../model/vocable-list';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../../services/auth.service';
+import { User } from '../../../model/internal/user';
+import { ComplexDialogManagementService } from '../../../services/complex-dialog-management.service';
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+    ownImports$!: Observable<VocableList[]>;
+
+    currentUser!: User;
+
+    constructor(
+        readonly vocabulary: VocabularyService,
+        private readonly auth: AuthService,
+        private readonly dialogManagement: ComplexDialogManagementService
+    ) {
+        this.auth.currentUser$.subscribe((user) => {
+            if (user) {
+                this.currentUser = user!;
+                this.ownImports$ = vocabulary.listsOfAuthor$(user!);
+            }
+        });
+    }
+
+    onVocabularyListSelected(list: VocableList): void {
+        this.dialogManagement.openVocableListDialog(list, this.currentUser);
+    }
+}
