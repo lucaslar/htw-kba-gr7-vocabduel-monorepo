76eded19e7d3407d7513e2bd8fc96188fa376722
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-24 06:25:03 +0200

    Enabled user dialog to be opened from running game card.

--- configuration.rest/src/main/htw-kba-gr7-vocabduel-frontend/src/app/components/main/dashboard/games/games.component.ts
+++ configuration.rest/src/main/htw-kba-gr7-vocabduel-frontend/src/app/components/main/dashboard/games/games.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { GameService } from '../../../../services/game.service';
 import { Observable } from 'rxjs';
 import { RunningGame } from '../../../../model/running-game';
 import { Router } from '@angular/router';
 import { NavigationService } from '../../../../services/navigation.service';
+import { ComplexDialogManagementService } from '../../../../services/complex-dialog-management.service';
+import { User } from '../../../../model/internal/user';
 
 @Component({
     selector: 'app-games',
@@ -12,17 +14,19 @@ import { NavigationService } from '../../../../services/navigation.service';
 })
 export class GamesComponent {
     readonly openGames$: Observable<RunningGame[]>;
+    @Input() currentUser!: User;
 
     constructor(
         readonly router: Router,
         readonly navigation: NavigationService,
-        private readonly gameService: GameService
+        private readonly gameService: GameService,
+        private readonly dialogManagement: ComplexDialogManagementService
     ) {
         this.openGames$ = gameService.openGames$;
     }
 
-    showOpponentDetails(): void {
-        // TODO: Implement
+    showOpponentDetails(opponent: User): void {
+        this.dialogManagement.openUserDialog(opponent, this.currentUser);
     }
 
     showGameDetails(): void {
